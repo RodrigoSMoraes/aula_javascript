634a03fb480ebb9349fe5fa945077560d3d0a54b
RodrigoSMoraes <rmoraes.engenharia@hotmail.com>
2021-04-08 22:00:47 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+//para matriz muda-se a concatenação >> ()para{}  >> = para:
+var fruta = [{nome:"maçã", cor:"varmelha"},{nome:"uva", cor:"verde"}]
+console.log(fruta[0].nome);
+alert(fruta[1].cor);
+
+
+//var lista = ["maçã","pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join("%"));
+//alert(lista.reverse())
+
+
+// comentário da linha
+/* comentário de trecho */
+
+//var nome = "Rodrigo Santos Moraes";
+//var idade1  = 30;
+//var idade2 = 20;
+//var frase = "Brasil campeão!"
+//alert("Bem vindo! " + nome + ", " + idade + " anos");
+//alert(idade1 + idade2);
+
+//console.log (nome);
+//console.log(idade1+idade2);
+//console.log(frase.toLocaleUpperCase("Brasil" , "Japão"));;
+//alert (frase.replace("Brasil" , "Japão"));
+
+
