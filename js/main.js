7f3afd99fe6c1107529cae379db9be8870674edd
RodrigoSMoraes <rmoraes.engenharia@hotmail.com>
2021-04-11 17:57:01 -0300

    curso finalizado

--- js/main.js
+++ js/main.js
@@ -1,8 +1,79 @@
+function clickou(){
+    document.getElementById("agradecimento");
+    console.log(document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clickar</b>");
+    //alert("obrigado por clickar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/RodrigoSMoraes/Recriando-a-pagina-inicial-do-instagram");
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML="obrigado por passar o mouse"
+    alert("trocar texto");
+}
+
+/*function soma ( n1,n2){
+    return n1 + n2;
+}
+
+
+function setReplace(frase, nome, sobrenome){
+    return frase.replace(nome, sobrenome)
+}*/
+/*var validar = 0;
+
+function validaidade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("qual sua idade");
+validaridade(idade);
+console.log(validaridade(idade));
+*/
+/*
+alert(soma(5,10));
+alert(setReplace("Rodrigo Santos", "Santos", "Moraes"));
+*/
+//var var1= "tx1";
+//var var2 = "txt2"
+
+//console.log(var1 + var2 + "1425""-""1519");
+
+//Trabalhar com Datas
+//var d = new Date();
+//    alert(d.getMonth()+1);
+
+//var count;
+//for(count=0;count <=5;count++){
+//    alert(count);
+//};
+
+/*var count = 5;
+    alert("contador"+count);
+while(count>1){
+    count--
+    alert("contador" + count);
+}*/
+
+/*var idade = prompt("qual sua idade?");
+//var idade = 19;
+if (idade >= 18){
+    alert("Maior de idade" + idade + "  anos");
+}else{
+        alert("Menor de idade");
+    }
+*/
 
 //para matriz muda-se a concatenação >> ()para{}  >> = para:
-var fruta = [{nome:"maçã", cor:"varmelha"},{nome:"uva", cor:"verde"}]
-console.log(fruta[0].nome);
-alert(fruta[1].cor);
+//var fruta = [{nome:"maçã", cor:"varmelha"},{nome:"uva", cor:"verde"}]
+//console.log(fruta[0].nome);
+//alert(fruta[1].cor);
 
 
 //var lista = ["maçã","pera","laranja"];
